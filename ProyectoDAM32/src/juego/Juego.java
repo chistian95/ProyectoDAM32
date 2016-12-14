578e70425fa1af5c4b457bad87d014e06a9f6476
chistian95 <kriskorral59@gmail.com>
2016-12-14 14:30:11 +0100

    Quitar codigo online

--- ProyectoDAM32/src/juego/Juego.java
+++ ProyectoDAM32/src/juego/Juego.java
@@ -7,14 +7,12 @@ import juego.jugador.Jugador;
 import juego.jugador.JugadorHumano;
 import juego.jugador.TurnoJugador;
 import juego.mapa.Generador;
-import network.cliente.Cliente;
-import network.servidor.Servidor;
 import pantalla.Pantalla;
 import pantalla.Renderizador;
 
 public class Juego extends Thread {
 	public static void main(String[] args) {
-		new Juego(true);
+		new Juego();
 	}
 	
 	private Pantalla pantalla;
@@ -23,42 +21,23 @@ public class Juego extends Thread {
 	private JugadorHumano jugador;
 	private EstadoJuego estadoJuego;
 	private List<Jugador> jugadores;
-	private boolean isServidor;
-	private Cliente cliente;
-	private Servidor servidor;
-	private BundleCliente bundleCliente;
 	
 	public Juego() {
-		this(false);
-	}
-	
-	public Juego(boolean isServidor) {
-		this.isServidor = isServidor;
-		start();
-	}
-	
-	public void run() {
 		estadoJuego = EstadoJuego.PRECARGA;
-		pantalla = new Pantalla(this);
-		bundleCliente = new BundleCliente(this);
-		if(isServidor) {			
-			generador = new Generador(this);
-			render = new Renderizador(this);
-			
-			try {
-				generador.join();
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			
-			estadoJuego = EstadoJuego.VISTA_MUNDO;
-			jugadores = new ArrayList<Jugador>();
-			jugador = new JugadorHumano(this);
-			jugadores.add(jugador);
-			servidor = new Servidor(this);
-		} else {	
-			cliente = new Cliente(this);
-		}		
+		pantalla = new Pantalla(this);			
+		generador = new Generador(this);
+		render = new Renderizador(this);
+		
+		try {
+			generador.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		estadoJuego = EstadoJuego.VISTA_MUNDO;
+		jugadores = new ArrayList<Jugador>();
+		jugador = new JugadorHumano(this);
+		jugadores.add(jugador);
 	}
 	
 	public void crearNaciones() {
@@ -121,21 +100,5 @@ public class Juego extends Thread {
 	
 	public List<Jugador> getJugadores() {
 		return jugadores;
-	}
-	
-	public Servidor getServidor() {
-		return servidor;
-	}
-	
-	public Cliente getCliente() {
-		return cliente;
-	}
-	
-	public boolean isServidor() {
-		return isServidor;
-	}
-	
-	public BundleCliente getBundleCliente() {
-		return bundleCliente;
 	}
 }
